a767c9f7e4c7222e2db9bf5ee0cfb9f7742deefa
kevin-seligmann <seligmann.kevin.cc@gmail.com>
2024-10-31 16:10:08 +0100

    Agregado algo de logica de inicio de sesion. Cambiado para servir en /public

--- web/controller/loginController.php
+++ web/controller/loginController.php
@@ -1,5 +1,26 @@
 <?php
 
-$error = "Login no implementado";
+session_start();
 
-require __DIR__.'/../view/error.php';
+require_once __DIR__.'/../util/Session.php';
+
+// Si ya existe una sesion, redireccionar a pagina principal
+if (isset($_SESSION['userSession'])){
+	header("Location: /index.php");
+}
+
+// Si el usuario y contraseña existen en el metodo POST, venimos de intento de acceder
+// Si no, venimos de un intento de registro nuevo
+if (isset($_POST['username']) && isset($_POST['password'])){
+	try {
+		// Intentamos validar los datos. Si son validos vamos a la pagina principal
+		$_SESSION['userSession'] = new Session($_POST['username'], $_POST['password']);
+		header("Location: /index.php");
+	} catch (LoginException $e){
+		// Si encontramos un error vamos a login con mensaje de error
+		$loginError = "Error: ".$e->getMessage();
+		require __DIR__.'/../view/login.php';
+	}
+} else {
+	require __DIR__.'/../view/login.php';
+}
--- web/public/index.php
+++ web/public/index.php
@@ -1,3 +1,13 @@
 <?php
-require '../controller/homeController.php';
-routeHomepage();
+
+if (isset($_GET['request'])){
+	$request = $_GET['request'];
+}
+else {
+	$request = 'homepage';
+}
+
+switch($request){
+	case 'homepage': require __DIR__.'/../controller/homeController.php'; break;
+	case 'login': require __DIR__.'/../controller/loginController.php'; break;
+}
