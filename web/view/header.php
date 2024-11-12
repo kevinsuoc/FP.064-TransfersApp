7bf35f2c6f7fb7436b48d0e2027b78d7cfe63335
Elena <elopezestr@uoc.edu>
2024-11-12 19:16:21 +0100

    pagina de bienvenida/login done

--- web/view/header.php
+++ web/view/header.php
@@ -13,5 +13,5 @@
 <div class="main-container">
     <!-- Header con franja de color azul claro semitransparente -->
     <header class="header-bar">
-    <h2>Codecrafters Airlines</h2>    
+<h2 class="airlines-title">CODECRAFTER AIRLINES</h2>    
     </header>
--- web/view/login.php
+++ web/view/login.php
@@ -1,6 +1,7 @@
 
-<div class="login-form">
-<form action="/" method="post"  >
+
+<div class="form-container">
+<form action="/" method="post"class="login-form"  >
   <div class="form-group">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
     <input type="text" class="form-control" name="username"placeholder="nombre de usuario" id="exampleInputEmail1" aria-describedby="emailHelp" required><br>
@@ -11,14 +12,16 @@
     <input type="password" class="form-control" id="exampleInputPassword1" name= "password" placeholder="contraseña" required><br>
    
   </div>
+  <div class="d-flex justify-content-around">
   <input type="hidden" name="request" value="login">
-  <button type="submit" value="login" class="btn btn-primary">Login</button>
+  <button type="submit" value="login" class="btn-bd-primary">Login</button>
   
 </form>
 <form action="/">
     <input type="hidden" name="request" value="registrarse"> 
-    <button type="submit" value="registrarse" class="btn btn-primary">Registrarse</button> <br>
+    <button type="submit" value="registrarse" class="btn-bd-primary">Registrarse</button> <br>
 </form>
+</div>
 </div>
 </div>
 <?php if (isset($loginError)){echo $loginError;}; ?>
