27ebc83a57f23894cbeb904d80b0cffb012a4fd5
Elena <elopezestr@uoc.edu>
2024-11-12 18:21:49 +0100

    front main page algo más presentabñe

--- web/public/index.php
+++ web/public/index.php
@@ -1,22 +1,47 @@
 <?php
-
 require_once __DIR__.'/../util/Session.php';
-
 session_start();
+?>
 
-if (isset($_REQUEST['request'])){
-	$request = $_REQUEST['request'];
-}
-else {
-	$request = 'homepage';
-}
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TransfersApp</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="styles.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    
+</head>
+<body>
+    <?php
+    include __DIR__ . '/../view/header.php';
 
-switch($request){
-	case 'registrarse':
-	case 'login':
-	case 'logout': require __DIR__.'/../controller/loginController.php'; break;
-	case 'reservar':
-	case 'reserva': require __DIR__.'/../controller/reservaController.php'; break;
-	case 'homepage':
-	default: require __DIR__.'/../controller/homeController.php';
-}
+    if (isset($_REQUEST['request'])){
+        $request = $_REQUEST['request'];
+    } else {
+        $request = 'homepage';
+    }
+
+    switch($request) {
+        case 'registrarse':
+        case 'login':
+        case 'logout': 
+            require __DIR__.'/../controller/loginController.php'; 
+            break;
+        case 'reservar':
+        case 'reserva': 
+            require __DIR__.'/../controller/reservaController.php'; 
+            break;
+        case 'homepage':
+        default: 
+            require __DIR__.'/../controller/homeController.php';
+    }
+    ?>
+</body>
+</html>
+
--- web/view/login.php
+++ web/view/login.php
@@ -1,19 +1,12 @@
-<!doctype html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-    <title>Login</title>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-  </head>
-<form action="/" method="post">
-  <div class="mb-3">
+
+<div class="login-form">
+<form action="/" method="post"  >
+  <div class="form-group">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
     <input type="text" class="form-control" name="username"placeholder="nombre de usuario" id="exampleInputEmail1" aria-describedby="emailHelp" required><br>
     
   </div>
-  <div class="mb-3">
+  <div class="form-group">
     <label for="exampleInputPassword1" class="form-label">Password</label>
     <input type="password" class="form-control" id="exampleInputPassword1" name= "password" placeholder="contraseña" required><br>
    
@@ -26,6 +19,7 @@
     <input type="hidden" name="request" value="registrarse"> 
     <button type="submit" value="registrarse" class="btn btn-primary">Registrarse</button> <br>
 </form>
-
+</div>
+</div>
 <?php if (isset($loginError)){echo $loginError;}; ?>
 <?php if (isset($loginMessage)){echo $loginMessage;}; ?>
