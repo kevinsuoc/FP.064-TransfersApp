5f711178574898ae6472bf1519e8a62c201dda7a
kevin seligmann <kseligmann@uoc.edu>
2024-11-28 07:54:00 +0100

    Controlador de precios - Menos validacion

--- producto 3/web/app/Http/Controllers/PrecioController.php
+++ producto 3/web/app/Http/Controllers/PrecioController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Hotel;
+use App\Models\Precio;
+use App\Models\Vehiculo;
 use Illuminate\Http\Request;
 
 // Mas detalles acerca de cada método se encuentran en la clase Zona (Son análogos) y documentación.
@@ -10,28 +13,51 @@ class PrecioController extends Controller
 {
     public function index()
     {
-        return view('admin.panel.precio.index');    
+        $precios = Precio::all();
+        return view('admin.panel.precio.index', ['precios' => $precios]);    
     }
 
     public function create()
     {
+        $vehiculos = Vehiculo::all();
+        $hoteles = Hotel::all();
+        return view ('admin.panel.precio.create', ['vehiculos' => $vehiculos, 'hoteles' => $hoteles]);
     }
 
     public function store(Request $request)
     {
-
+        $this->validar($request);
+        $this->setData($request, new Precio());
+        return redirect()->route('precio.index')->with('success', 'Precio creado');
     }
 
     public function edit(string $id)
     {
-
+        $vehiculos = Vehiculo::all();
+        $hoteles = Hotel::all();
+        $precio = Precio::find($id);
+        return view ('admin.panel.precio.edit', ['precio' => $precio, 'vehiculos' => $vehiculos, 'hoteles' => $hoteles]);
     }
 
     public function update(Request $request, string $id)
     {
+        $this->validar($request);
+        $this->setData($request, Precio::find($id)); 
     }
 
     public function destroy(string $id)
     {
+        Precio::destroy($id);
+        return redirect()->route('precio.index')->with('success', 'Precio eliminado');
+    }
+
+    private function validar(Request $request){
+        $request->validateWithBag('validacion', []);
+    }
+
+    private function setData(Request $request, Precio $precio){
+        $precio->id_hotel = $request->id_hotel;
+        $precio->id_vehiculo = $request->id_vehiculo;
+        $precio->save();
     }
 }
