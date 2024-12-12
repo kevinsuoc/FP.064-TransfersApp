4a0224946756b191a985d229fd6807c85274be72
Elena <elopezestr@uoc.edu>
2024-12-12 10:06:09 +0100

    precios ya está

--- producto 3/web/resources/views/panel/precio/edit.blade.php
+++ producto 3/web/resources/views/panel/precio/edit.blade.php
@@ -2,14 +2,15 @@
 
 <div class = "main-container">
     @include('header')
-    <p>Editar precio</p>
+    <div class="container mt-5 p-4 shadow-sm rounded" style="max-width: 600px; background: #f8f9fa;">
+    <h2 class="mb-4 text-center">Editar precio</h2>
 
     <div>
         <form action="{{route('precio.update',$precio->id_precio)}}" method="POST">
             @csrf
             @method('PUT')
 
-
+            <div class="form-group">
             <select name="id_hotel" id="id_hotel" required>
             <option value="" disabled selected>Selecciona una hotel</option>
             @foreach ($hoteles as $hotel)
@@ -19,7 +20,8 @@
             @error('id_hotel', 'validacion')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
-
+            </div>
+            <div class="form-group mt-3">
             <select name="id_vehiculo" id="id_vehiculo" required>
             <option value="" disabled selected>Selecciona un vehiculo</option>
             @foreach ($vehiculos as $vehiculo)
@@ -29,7 +31,8 @@
             @error('id_vehiculo', 'validacion')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
-
+            </div>
+            <div class="form-group mt-3">
             <label for="precio">Precio</label>
             <input value="{{$precio->precio}}" type="number" step="0.01" name="precio" id="precio" required>
             @error('precio', 'validacion')
@@ -38,8 +41,8 @@
             @error('precio_unico', 'validacion')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
-
-            <button type="submit">Aceptar</button>
+            </div>
+            <button type="submit"class="btn-bd-primary mt-4">Aceptar</button>
             @if (session('success'))
                 <div class="alert alert-success">
                     {{ session('success') }}
@@ -48,7 +51,8 @@
         </form>
         <form action="{{ route('precio.index') }}" class="list-group-item">
                 @csrf
-                <button type="submit" class="btn btn-info w-100">Panel precios</button>
+                <button type="submit" class="btn-bd-primary w-100">Panel precios</button>
         </form>
     </div>
+</div>
 </div>
