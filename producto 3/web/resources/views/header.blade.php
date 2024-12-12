ccf9139b7826fd698ea6f6c10d0f1ad7d6999f28
Elena <elopezestr@uoc.edu>
2024-12-12 11:37:13 +0100

    commit vehiculo ya está

--- producto 3/web/resources/views/header.blade.php
+++ producto 3/web/resources/views/header.blade.php
@@ -1,6 +1,6 @@
 <header class="header-bar d-flex justify-content-between align-items-center">
 
-    <h2 class="airlines-title">CODECRAFTER AIRLINES</h2> 
+    <h2 class="airlines-title">CODECRAFTER TRANSFERS</h2> 
 
     <div>
         <a href="/" class="btn btn-primary">Homepage</a>
--- producto 3/web/resources/views/panel/vehiculo/index.blade.php
+++ producto 3/web/resources/views/panel/vehiculo/index.blade.php
@@ -2,7 +2,8 @@
 
 <div class = "main-container">
     @include('header')
-    <p>Vehiculos</p>
+    <div class="container mt-5 p-4 shadow-sm rounded" style="max-width: 400px; background: #f8f9fa;">
+    <h2 class="mb-4 text-center">Vehículos</h2>
 
     @if (session('success'))
         <div class="alert alert-success">
@@ -12,23 +13,25 @@
 
     <form action="{{route('vehiculo.create')}}">
             @csrf
-            <button type="submit">Agregar vehiculo</button>
+            <button type="submit"class="btn-bd-primary">Agregar vehiculo</button>
     </form>
 
     @foreach ($vehiculos as $vehiculo)
     <div>
+    <div class="mt-4">
             <p><strong>Descripción: </strong>{{$vehiculo->descripcion}}</p>
             <p><strong>Email conductor: </strong>{{$vehiculo->email_conductor}}</p>
             <form action="{{route('vehiculo.edit',$vehiculo->id_vehiculo)}}">
                 @csrf
-                <button type="submit">Editar</button>
+                <button type="submit"class="btn-bd-primary">Editar</button>
             </form>
             <form action="{{route('vehiculo.destroy', $vehiculo->id_vehiculo)}}" method="POST">
                 @csrf
                 @method('DELETE')
-                <button type="submit">Borrar</button>
+                <button type="submit"class="btn-bd-primary">Borrar</button>
             </form>
     </div><br>
     @endforeach
 
+</div>
 </div>
