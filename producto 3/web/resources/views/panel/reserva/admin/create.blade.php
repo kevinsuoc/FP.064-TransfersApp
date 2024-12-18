d6439069ce9ff61679e4ad25c458f650729f563e
kevinsuoc <kseligmann@uoc.edu>
2024-12-18 18:47:05 +0100

    Reserva admin arreglado

--- producto 3/web/resources/views/panel/reserva/admin/create.blade.php
+++ producto 3/web/resources/views/panel/reserva/admin/create.blade.php
@@ -2,7 +2,7 @@
 
 <div class="main-container">
 @include('header')
-<div class="container mt-5 p-4 shadow-sm rounded" style="max-width: 850px; background: #f8f9fa;">
+<div class="container mt-5 p-4 shadow-sm rounded" style="max-width: 600px; background: #f8f9fa;">
     <h2 class="mb-4 text-center">Crear reserva</h2>
     <form action="{{route('reserva.store')}}" method="POST">
         @csrf
@@ -14,7 +14,7 @@
                 @endforeach
             </select>
 
-            <div id="todos_los_tipos" class="p-3 mb-4 border rounded" hidden>
+            <div id="todos_los_tipos" class="p-3 mb-4 border rounded w-100" hidden>
                 <h3 class="mb-3">Datos generales</h3>
                 <label for="email_cliente" class="form-label">Email del cliente</label>
                 <input type="email" name="email_cliente" id="email_cliente" value="{{old('email_cliente')}}" class="form-control mb-3">
@@ -39,31 +39,61 @@
                 </select>
             </div>
 
-            <div id="ida" class="p-3 mb-4 border rounded" hidden>
+            <div id="ida" class="p-3 mb-4 border rounded w-100" hidden>
                 <h3 class="mb-3">Datos de ida</h3>
                 <label for="fecha_entrada" class="form-label">Fecha de llegada del vuelo</label>
-                <input id="fecha_entrada" name="fecha_entrada" type="date" value="{{old('fecha_entrada')}}" class="form-control mb-3">
+                <input class="form-control" id="fecha_entrada" name="fecha_entrada" type="date" value="{{old('fecha_entrada')}}" class="form-control mb-3">
                 @error('fecha_entrada', 'validacion')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
 
                 <label for="hora_entrada" class="form-label">Hora de llegada del vuelo</label>
-                <input id="hora_entrada" name="hora_entrada" type="time" value="{{old('hora_entrada')}}" class="form-control mb-3">
+                <input class="form-control" id="hora_entrada" name="hora_entrada" type="time" value="{{old('hora_entrada')}}" class="form-control mb-3">
                 @error('hora_entrada', 'validacion')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
-            </div>
 
-            <div id="vuelta" class="p-3 mb-4 border rounded" hidden>
-                <h3 class="mb-3">Datos de vuelta</h3>
-                <label for="fecha_salida" class="form-label">Fecha de salida del vuelo</label>
-                <input id="fecha_salida" name="fecha_salida" type="date" value="{{old('fecha_salida')}}" class="form-control mb-3">
-                @error('fecha_salida', 'validacion')
+                <label for="numero_vuelo_entrada">Numero del vuelo de entrada</label>
+                <input class="form-control" id="numero_vuelo_entrada" name="numero_vuelo_entrada" value="{{old('numero_vuelo_entrada')}}" type="text" ><br>
+                @error('numero_vuelo_entrada', 'validacion')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+
+                <label for="origen_vuelo_entrada">Origen del vuelo de entrada</label>
+                <input class="form-control" id="origen_vuelo_entrada" name="origen_vuelo_entrada" value="{{old('origen_vuelo_entrada')}}" type="text" ><br>
+                @error('origen_vuelo_entrada', 'validacion')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
             </div>
 
-            <button type="submit" class="btn-bd-primary ">Crear</button>
+            <div id="vuelta" class="p-3 mb-4 border rounded w-100" hidden>
+                <h3 class="mb-3">Datos de vuelta</h3>
+                <label for="fecha_salida" class="form-label">Fecha de salida del vuelo</label>
+                <input class="form-control" id="fecha_salida" name="fecha_salida" type="date" value="{{old('fecha_salida')}}" class="form-control mb-3">
+                @error('fecha_salida', 'validacion')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+                        
+                <label for="hora_salida">Hora de salida del vuelo</label>   
+                <input class="form-control" id="hora_salida" name="hora_salida" value="{{old('hora_salida')}}" type="time" ><br>
+                @error('hora_salida', 'validacion')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+                
+                <label for="numero_vuelo_salida">Numero del vuelo de salida</label>
+                <input class="form-control" id="numero_vuelo_salida" name="numero_vuelo_salida" value="{{old('numero_vuelo_salida')}}" type="text" ><br>
+                @error('numero_vuelo_salida', 'validacion')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+
+                <label for="hora_recogida">Hora de recogida del hotel</label>
+                <input class="form-control" id="hora_recogida" name="hora_recogida" value="{{old('hora_recogida')}}" type="time" ><br>
+                @error('hora_recogida', 'validacion')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <button type="submit" class="btn-bd-primary  w-100">Crear</button>
         </div>
     </form>
 
