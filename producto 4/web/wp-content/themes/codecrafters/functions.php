cc249f3facb3816c77407d7f784358d8edf2bd4e
kevinsuoc <kseligmann@uoc.edu>
2025-01-03 15:31:02 +0100

    Headr y footer personalizados

--- producto 4/web/wp-content/themes/codecrafters/functions.php
+++ producto 4/web/wp-content/themes/codecrafters/functions.php
@@ -32,10 +32,50 @@ function oceanwp_child_enqueue_parent_style() {
 
 add_action( 'wp_enqueue_scripts', 'oceanwp_child_enqueue_parent_style' );
 
-add_action('wp_head', 'my_custom_styles_and_scripts');
 
-function my_custom_styles_and_scripts() {
+function load_styles() {
   ?>
 	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css" type="text/css" media="all">
   <?php
 }
+add_action( 'wp_head', 'load_styles');
+
+
+function footer_hook() {
+    ?>
+	<div class="footer-container">
+		<div class="container custom-footer">
+			<div class="footer-nav">
+				<p><strong>Sitio Web</strong><a href="https://fp064.techlab.uoc.edu/~uocx2/producto3/" class="footer-link">CodeCrafters Transfers</a></p>
+				<p><strong>Soporte</strong> soportecliente@cct.com, 902202122</p>
+				<p><strong>Corporativo</strong> corporativo@cct.com, 902202123</p>
+			</div>
+		</div>
+	</div>
+	<div class="footer-container footer-copyright">
+		<p> Desarrollado por CodeCrafters </p>
+	</div>
+    <?php
+}
+add_action('ocean_footer', 'footer_hook', 10);
+
+function header_hook(){
+	?>
+
+			<div class="header-container">
+				<div class="container custom-header">
+					<img width="1215" height="182" src="http://localhost/wp-content/themes/codecrafters/assets/logo.png" class="custom-logo">
+				</div>
+			</div>
+
+			<div class="nav-background">
+				<div class="header-nav">
+					<div class="link-container"><a href="/">HOME</a></div>
+					<div class="link-container"><a href="nuestros-servicios">NUESTROS SERVICIOS</a></div>
+					<div class="link-container"><a href="nuestra-flota">NUESTRA FLOTA</a></div>
+					<div class="link-container"><a href="noticias">NOTICIAS</a></div>
+				</div>
+			</div>
+	<?php
+}
+add_action('ocean_header', 'header_hook', 10);
